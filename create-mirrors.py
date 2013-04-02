d240f71a35e4b23e686a1aafd91e8368ac90658d
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-04-02 00:22:00 -0500

    add script to parse default.xml into mirror-{client,host}.xml
    Change-Id: If42876d0aacc411b6cfbed16a80728fa2ff185f2

---
+++ create-mirrors.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2.7
+import xml.etree.ElementTree as ET
+
+
+tree = ET.parse('default.xml')
+root = tree.getroot()
+
+for project in root:
+    if project.tag == 'project':
+        project.attrib.pop('path')
+        if project.get('groups'):
+          project.attrib.pop('groups')
+    elif project.tag == 'remote':
+        project.attrib.pop('review')
+    for copyfile in project:
+        if copyfile.tag == 'copyfile':
+            project.remove(copyfile)
+
+tree.write('mirror-host.xml')
+
+tree = ET.parse('default.xml')
+root = tree.getroot()
+
+for project in root:
+    if project.tag == 'project':
+        if project.get('remote'):
+            project.attrib.pop('remote')
+    elif project.tag == 'remote':
+        if project.get('name') == 'github':
+            root.remove(project)
+        elif project.get('name') == 'aosp':
+            project.set('fetch','..')
+
+tree.write('mirror-client.xml')
+
