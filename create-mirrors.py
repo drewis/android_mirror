a2adb22d6020f3ffa71c86f37f5706e863d4b5d4
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-04-18 15:33:06 -0500

    changes
    Change-Id: I570e9329913303bbdd5104fea9d5940dc2cf8f9a

--- create-mirrors.py
+++ create-mirrors.py
@@ -9,7 +9,7 @@ for project in root:
     if project.tag == 'project':
         project.attrib.pop('path')
         if project.get('groups'):
-          project.attrib.pop('groups')
+            project.attrib.pop('groups')
     elif project.tag == 'remote':
         project.attrib.pop('review')
     for copyfile in project:
@@ -25,10 +25,13 @@ for project in root:
     if project.tag == 'project':
         if project.get('remote'):
             project.attrib.pop('remote')
+        if project.get('groups'):
+            project.attrib.pop('groups')
     elif project.tag == 'remote':
         if project.get('name') == 'github':
             root.remove(project)
         elif project.get('name') == 'aosp':
+            project.attrib.pop('review')
             project.set('fetch','.')
 
 tree.write('mirror-client.xml')
