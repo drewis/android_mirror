f502a3176eb2fb2d28ef96c4e14e4feb3addb96a
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-07-18 11:48:24 -0500

    fix branches and special cases for unique ev projects
    Change-Id: I63ddfefd7b0b865c2a6ba3bad12b614b87831206

--- add-revision-to-aosp.py
+++ add-revision-to-aosp.py
@@ -13,13 +13,15 @@ for project in root:
         aosp_repos.append(project.get('name'))
 
 # Extract all projects from ev mirror manifest
-tree = ET.parse('default.xml')
+tree = ET.parse('local_mirror_manifest.xml')
 root = tree.getroot()
 
 ev_repos = []
+ev_repos_branches = {}
 for project in root:
     if project.tag == 'project':
         ev_repos.append(project.get('name'))
+        ev_repos_branches[project.get('name')] = project.get('revision')
 
 # Sort just cause
 aosp_repos.sort()
@@ -31,6 +33,33 @@ ev_repos_converted = []
 for r in ev_repos:
     if r == 'android':
         ev_repos_converted.append('platform/manifest')
+        continue
+    elif r == 'android_external_fsck_msdos':
+        ev_repos_converted.append('platform/external/fsck_msdos')
+        continue
+    elif r == 'android_external_wpa_supplicant':
+        ev_repos_converted.append('platform/external/wpa_supplicant')
+        continue
+    elif r == 'android_external_wpa_supplicant_6':
+        ev_repos_converted.append('platform/external/wpa_supplicant_6')
+        continue
+    elif r == 'android_external_wpa_supplicant_8':
+        ev_repos_converted.append('platform/external/wpa_supplicant_8')
+        continue
+    elif r == 'android_hardware_libhardware_legacy':
+        ev_repos_converted.append('platform/hardware/libhardware_legacy')
+        continue
+    elif r == 'android_kernel_grouper':
+        ev_repos_converted.append('kernel/tegra')
+        continue
+    elif r == 'android_kernel_lge_mako':
+        ev_repos_converted.append('kernel/msm')
+        continue
+    elif r == 'android_kernel_samsung_manta':
+        ev_repos_converted.append('kernel/exynos')
+        continue
+    elif r == 'android_kernel_samsung_tuna':
+        ev_repos_converted.append('kernel/omap')
         continue
     r2 = r.lstrip('android').lstrip('_').replace('_','/')
     if r2.startswith('device') or r2.startswith('kernel'):
@@ -51,12 +80,17 @@ ev_unique = set(ev_repos_converted) - set(aosp_repos)
 # Remap unique ev projects back to real name
 ev_unique_remapped = [ r[0] for r in ev_mapping if r[1] in ev_unique ]
 ev_unique_remapped.sort()
+
 # Write unique projects to new manifest for reference
 ev_manifest = "ev_unique.xml"
+def_remote = "ev"
+def_revision = "jellybean"
+def_fetch_url = "https://github.com/Evervolv/"
 root = ET.Element("manifest")
+ET.SubElement(root, "remote", name="%s" % def_remote, fetch="%s" % def_fetch_url)
 
 for r in ev_unique_remapped:
-    ET.SubElement(root, "project", name="%s" % r,remote="ev",revision="jellybean")
+    ET.SubElement(root, "project", name="%s" % r,remote="ev",revision="%s" % ev_repos_branches.get(r))
 
 tree = ET.ElementTree(root)
 tree.write(ev_manifest,encoding="UTF-8", xml_declaration=True)
@@ -67,36 +101,39 @@ xml = md.parse(ev_manifest)
 with open(ev_manifest, 'w') as f:
     f.write(xml.toprettyxml())
 
-failed = []
-# Create symbolic links for ev shared projects
-# to the aosp naming so mirroring will work correctly
-for s in shared:
-    print "linking %s.git -> %s.git" % (ev_mapping_rev_dict.get(s),s)
-    try:
-        os.symlink("%s.git" % s,"%s.git" % ev_mapping_rev_dict.get(s))
-    except:
-        failed.append(s)
+# For setting up combined mirror
+symlinks = False
+if symlinks:
+    failed = []
+    # Create symbolic links for ev shared projects
+    # to the aosp naming so mirroring will work correctly
+    for s in shared:
+        print "linking %s.git -> %s.git" % (ev_mapping_rev_dict.get(s),s)
+        try:
+            os.symlink("%s.git" % s,"%s.git" % ev_mapping_rev_dict.get(s))
+        except:
+            failed.append(s)
 
-cur_dir = os.getcwd()
-for s in shared:
-    os.chdir(s+".git")
-    print "Adding ev remote %s to %s" % (ev_mapping_rev_dict.get(s),s)
-    subprocess.call(['git','remote','add','ev','--no-tags',"https://github.com/Evervolv/%s"
-        % ev_mapping_rev_dict.get(s)])
-    print "Fixing ev remote to pull to ev/*"
-    subprocess.call(['git','config','remote.ev.fetch','+refs/heads/*:refs/heads/ev/*'])
-    print "Adding cm remote %s to %s (no tags)" % (ev_mapping_rev_dict.get(s),s)
-    subprocess.call(['git','remote','add','cm','--no-tags',"https://github.com/CyanogenMod/%s"
-        % ev_mapping_rev_dict.get(s)])
-    print "Fixing cm remote to pull to cm/*"
-    subprocess.call(['git','config','remote.cm.fetch','+refs/heads/*:refs/heads/cm/*'])
-    print "Adding caf remote %s to %s (no tags)" % (s,s)
-    subprocess.call(['git','remote','add','caf','--no-tags','git://codeaurora.org/%s'
-        % s ])
-    print "fixing caf remote to pull to caf/*"
-    subprocess.call(['git','config','remote.caf.fetch','+refs/heads/*:refs/heads/caf/*'])
+    cur_dir = os.getcwd()
+    for s in shared:
+        os.chdir(s+".git")
+        print "Adding ev remote %s to %s" % (ev_mapping_rev_dict.get(s),s)
+        subprocess.call(['git','remote','add','ev','--no-tags',"https://github.com/Evervolv/%s"
+            % ev_mapping_rev_dict.get(s)])
+        print "Fixing ev remote to pull to ev/*"
+        subprocess.call(['git','config','remote.ev.fetch','+refs/heads/*:refs/heads/ev/*'])
+        print "Adding cm remote %s to %s (no tags)" % (ev_mapping_rev_dict.get(s),s)
+        subprocess.call(['git','remote','add','cm','--no-tags',"https://github.com/CyanogenMod/%s"
+            % ev_mapping_rev_dict.get(s)])
+        print "Fixing cm remote to pull to cm/*"
+        subprocess.call(['git','config','remote.cm.fetch','+refs/heads/*:refs/heads/cm/*'])
+        print "Adding caf remote %s to %s (no tags)" % (s,s)
+        subprocess.call(['git','remote','add','caf','--no-tags','git://codeaurora.org/%s'
+            % s ])
+        print "fixing caf remote to pull to caf/*"
+        subprocess.call(['git','config','remote.caf.fetch','+refs/heads/*:refs/heads/caf/*'])
 
-    os.chdir(cur_dir)
+        os.chdir(cur_dir)
 
-print failed
+    print failed
 print "DONE"
