42b3bf7053a00e5cc3890e5f36bd7d2e2c7fe969
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-10-31 19:10:38 -0500

    script to set remotes/revisions on the aosp manifest
    Change-Id: Ida0ba3fbbafad0502adfe49c631172cd65c97804

---
+++ set-remote-revision-on-projects.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2.7
+import xml.etree.ElementTree as ET
+
+
+tree = ET.parse('kk.xml')
+root = tree.getroot()
+
+rem = "aosp"
+rev = "refs/tags/android-4.4_r1"
+
+for project in root:
+    if project.tag == 'project':
+        project.attrib['remote'] = rem
+        project.attrib['revision'] = rev
+
+tree.write('kk2.xml')
