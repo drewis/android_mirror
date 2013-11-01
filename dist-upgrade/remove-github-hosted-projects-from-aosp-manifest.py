15210b833f82acdab4545c53d2866afe8f840a1c
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-10-31 20:08:55 -0500

    run and fix removal script
    Change-Id: I30fa4f866cf92040b70fba654548cf887bd2635d

--- dist-upgrade/remove-github-hosted-projects-from-aosp-manifest.py
+++ dist-upgrade/remove-github-hosted-projects-from-aosp-manifest.py
@@ -3,7 +3,7 @@
 
 import xml.etree.ElementTree as ET
 
-old_xml = 'default.xml'
+old_xml = 'old.xml'
 new_xml = 'kk.xml'
 
 tree = ET.parse(old_xml)
@@ -13,14 +13,22 @@ hosted_projects = []
 for project in root:
     if project.tag == 'project':
         if project.get('groups') == 'ev-aosp':
-            hosted_projects.append(project.get('path')) # path here
+            hosted_projects.append(project.get('path'))
 
 tree = ET.parse(new_xml)
 root = tree.getroot()
 
+projects_to_remove = []
 for project in root:
     if project.tag == 'project':
         if project.get('path') in hosted_projects:
-            root.remove(project)
+            projects_to_remove.append(project.get('path'))
+
+for project in projects_to_remove:
+    for p in root:
+        if p.tag == 'project':
+            if p.get('path') == project:
+                root.remove(p)
+                break
 
 tree.write(new_xml)
